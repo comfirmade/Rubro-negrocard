5ef3ef44fa86ff704399d8f2de85416951422fe1
comfirmade <naua.lobo@gmail.com>
2024-10-27 15:55:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'Em qual ano o Flamengo foi fundado',
+    'O Flamengo foi fundado em 1895'
+)
+
+criaCartao(
+    'Geografia',
+    'Em qual cidade o Flamengo está localizado?',
+    'O Flamengo está em todo o país'
+)
+
+criaCartao(
+    'Títulos',
+    'Quantas vezes o Flamengo foi campeão mundial?',
+    'O Flamengo foi campeão mundial apenas uma vez em 1981'
+)
+
+criaCartao(
+    'História',
+    'Qual jogador do Flamengo teve o maior impacto na história do clube?',
+    'O Arthur Antunes de Coimbra mais conhecido por Zico'
+)
+
+criaCartao(
+    'História',
+    'Qual é o nome completo do Flamengo?',
+    'Clube de Regatas do Flamengo (CRF)'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o primeiro esporte a ser praticado no Clube de Regatas do Flamengo?',
+    'O remo'
+)
